f7604ed6927e8a0beddf58ff4a1d60660f01019e
alecmucz <alecmucz@gmail.com>
2024-03-06 20:54:49 -0500

    @author Alec Muczynski Implemented the Pending stage

--- src/main/java/com/example/hospitaldeliveryinterface/HelloController.java
+++ src/main/java/com/example/hospitaldeliveryinterface/HelloController.java
@@ -1,14 +1,43 @@
 package com.example.hospitaldeliveryinterface;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button addNoteButton;
+
+    @FXML
+    private Button completedButton;
+
+    @FXML
+    private Button deliverPackageButton;
+
+    @FXML
+    private Button editDeliveryButton;
+
+    @FXML
+    private Button newDeliveryButton;
+
+    @FXML
+    private Button pendingButton;
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private Button searchLocationButton;
+
+    @FXML
+    private Button searchMedicationButton;
+
+    @FXML
+    private Button searchNameButton;
+
+    @FXML
+    private MenuButton settingsButton;
+
 }
