5a9a5a9fd09b31b88bebb18f3f0b082858955cc1
Mitchell Lam <lammitch95@gmail.com>
2024-04-14 14:33:12 -0400

    alec deelete and multiselect

--- src/main/java/com/example/hospitaldeliveryinterface/model/MitchTextTranslate.java
+++ src/main/java/com/example/hospitaldeliveryinterface/model/MitchTextTranslate.java
@@ -97,7 +97,7 @@ public class MitchTextTranslate {
         languagesMap.put("Ukrainian", "uk");
 
 
-        //storedLang = DataBaseMgmt.initialLanguageCheck(languagesMap);
+        storedLang = DataBaseMgmt.initialLanguageCheck(languagesMap);
 
     }
 
